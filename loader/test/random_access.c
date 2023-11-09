b7d598bdbf271b86ec927f9efd42db0591e653ba
Trung Dang <bill081001@gmail.com>
2023-11-08 22:19:33 -0600

    random_access changes

--- loader/test/random_access.c
+++ loader/test/random_access.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define SIZE 268435456
-#define JUMP 1048576
+#define COUNT 1024
 
 int fib[SIZE];
 
+int position() {
+    return (rand() * rand()) & (SIZE - 1);
+}
+
 int main(int argc, char** argv) {
-    // fib = (int*)malloc(SIZE * sizeof(int))
-#define FIB(i) fib[(i) * JUMP]
-    FIB(0) = FIB(1) = 1;
-    for (int i = 2; i * JUMP < SIZE; i++) {
-        FIB(i) = FIB(i - 1) + FIB(i - 2);
+    srand(time(NULL));
+    int a = position(), b = position();
+    fib[a] = fib[b] = 1;
+    for (int i = 2; i < COUNT; i++) {
+        int c = position();
+        fib[c] = fib[a] + fib[b];
+        a = b; b = c;
     }
-    printf("%d\n", FIB((SIZE - 1) / JUMP));
+    printf("%d\n", fib[b]);
 }
